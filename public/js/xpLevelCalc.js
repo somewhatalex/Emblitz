be851f38823b0fbb7b084a1b88dcbde67c264dcb
Emba 696 <embabyrd6@gmail.com>
2022-08-28 01:39:07 -0400

    Added basis for equation

--- public/js/xpLevelCalc.js
+++ public/js/xpLevelCalc.js
@@ -1,7 +1,17 @@
 function getXpLevel(xp) {
-    
+    return Math.floor(/*xpToLevelEquation(xp)*/);
+}
+
+function getXpFromLevel(level){ //Inverse
+    return Math.round(Math.pow(2, (level*.1))*level);
 }
 
 function xpUntilNextLevel(xp, level) {
+    return Math.ceil(/*xpToLevelEquation(xp)*/)
+}
+
+function getRemainderXP(xp, level){
 
 }
+
+//I AM DIENG OF SLEEP DEPRIVATION PLZ HELP ME
