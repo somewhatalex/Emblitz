634cb4d98fc21fc9b6fb99eed8fb627ef16e5aa8
Alex <42502560+bob4koolest@users.noreply.github.com>
2022-10-09 12:29:07 -0400

    Delete mapper.js

--- public/js/mapper.js
+++
@@ -1,8 +0,0 @@
-function makemap(data) {
-    document.getElementById("maploading").innerHTML = "";
-    document.getElementById("svg-map-bg").innerHTML += data;
-    for(let i=0; i<document.getElementsByClassName("map-region").length; i++) {
-        document.getElementsByClassName("map-region")[i].setAttribute("data-color", "default");
-    }
-    initializeMap();
-}
