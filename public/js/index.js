61a808c8b8112ce0226dcb8c69d17d6dbb0ea615
Emba696 <embabyrd6@gmail.com>
2026-04-13 23:13:17 -0400

    Possibly fixed the touch screen issue.

--- public/js/index.js
+++ public/js/index.js
@@ -573,12 +573,11 @@ function initializeMap() {
         });
         document.addEventListener("touchmove", (e) => {
             //mobile panning
-            if(mapElement.contains(e.target) || e.target === gameScreen) {
-                if(playerGui.contains(e.target)) return;
+            if(mapElement.contains(e.target) || e.target === document.getElementById("gamescreen")) {
+                if(document.getElementById("player_gui").contains(e.target)) return;
                 isDragging = true;
-                let mapTranslateX = currentMapTranslateX;
-                let mapTranslateY = currentMapTranslateY;
-                let mapZoom = currentMapZoom;
+                let mapTranslate = mapElement.style.transform.replace(/\(/g, "").replace(/\)/g, "").replace(/translate/g, "").replace(/px/g, "").replace(/ /g, "").split(",");
+                let mapZoom = Number(document.getElementById("mapcontainer").style.transform.replace(/\(/g, "").replace(/\)/g, "").replace(/scale/g, ""));
 
                 let touch = e.touches[0];
                 if(previoustouch) {
@@ -591,26 +590,27 @@ function initializeMap() {
                 let deltaY = e.movementY/1.3;
 
                 //must check both x and y coords for scrolling
-                if(mapTranslateX > mapboundsX[0] && deltaX > 0) {
+                if(mapTranslate[0] > mapboundsX[0] && deltaX > 0) {
                     deltaX = 0;
-                } else if(mapTranslateX < mapboundsX[1] && deltaX < 0) {
+                } else if(mapTranslate[0] < mapboundsX[1] && deltaX < 0) {
                     deltaX = 0;
                 }
 
-                if(mapTranslateY > mapboundsY[0] && deltaY > 0) {
+                if(mapTranslate[1] > mapboundsY[0] && deltaY > 0) {
                     deltaY = 0;
-                } else if(mapTranslateY < mapboundsY[1] && deltaY < 0) {
+                } else if(mapTranslate[1] < mapboundsY[1] && deltaY < 0) {
                     deltaY = 0;
                 }
-                currentMapTranslateX = Number(mapTranslateX) + deltaX/mapZoom;
-                currentMapTranslateY = Number(mapTranslateY) + deltaY/mapZoom;
-                mapElement.style.transform = `translate(${currentMapTranslateX}px, ${currentMapTranslateY}px)`;
+                if(!mapTranslate[1]) {
+                    mapTranslate[1] = 0;
+                }
+                mapElement.style.transform = `translate(${Number(mapTranslate[0]) + deltaX/mapZoom}px, ${Number(mapTranslate[1]) + deltaY/mapZoom}px)`;
             }
 
             //mobile zooming
-            if(zoomElement.contains(e.target) || e.target === gameScreen) {
+            if(zoomElement.contains(e.target) || e.target === document.getElementById("gamescreen")) {
                 if(!e.touches[1]) return;
-                if(playerGui.contains(e.target)) return;
+                if(document.getElementById("player_gui").contains(e.target)) return;
 
                 let currentzoom = Math.sqrt((e.touches[1].pageX - e.touches[0].pageX)**2 + (e.touches[1].pageY - e.touches[0].pageY)**2);
                 if(previousmobilezoom) {
@@ -618,7 +618,7 @@ function initializeMap() {
                 }
                 previousmobilezoom = currentzoom;
 
-                let elementTransform = currentMapZoom;
+                let elementTransform = Number(zoomElement.style.transform.replace(/\(/g, "").replace(/\)/g, "").replace(/scale/g, ""));
                 var zoomdelta = (e.deltaY)/250;
                 if(zoomdelta > 0.3) {
                     zoomdelta = 0.3;
@@ -629,22 +629,15 @@ function initializeMap() {
                     if(zoomdelta > 0) {
                         zoomdelta = 0;
                     }
-                    elementTransform = 1.7;
+                    zoomElement.style.transform = "scale(1.7)";
                 } else if(elementTransform <= 0.5) {
                     if(zoomdelta < 0) {
                         zoomdelta = 0;
                     }
-                    elementTransform = 0.5;
+                    zoomElement.style.transform = "scale(0.5)";
                 }
 
-                elementTransform += zoomdelta;
-                if(elementTransform > 1.7) {
-                    elementTransform = 1.7;
-                } else if(elementTransform < 0.5) {
-                    elementTransform = 0.5;
-                }
-                currentMapZoom = elementTransform;
-                zoomElement.style.transform = `scale(${currentMapZoom})`;
+                zoomElement.style.transform = `scale(${elementTransform += zoomdelta})`;
             }
         });
         document.addEventListener("pointermove", function(e) {
